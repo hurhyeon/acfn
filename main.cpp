442fef59488ce277dda3a518d6059760b9d7bbd1
hurhyeon <90999300+hurhyeon@users.noreply.github.com>
2023-07-30 12:22:29 +0900

    fb1-5.l

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
-// main.cpp
-#include <stdio.h>
-#include "sum.h"
+/* recognize tokens for the calculator and print them out */
+%{
+#include "fb1-5.tab.h"
+%}
 
-int main() {
-  int s = sum(10);
-  printf("result=%d\n", s);
-}
-
+%%
+"+" { return ADD; }
+"-" { return SUB; }
+"*" { return MUL; }
+"/" { return DIV; }
+"|" { return ABS; }
+[0-9]+(\.[0-9]+)? { yylval.floatval = atof(yytext); return NUMBER; }
+\n { return EOL; }
+"(" { return OP; }
+")" { return CP; }
+"//".* /* ignore comments */
+[ \t] { /* ignore whitespace */ }
+. { printf("Mystery character %c\n", *yytext); }
+%%
